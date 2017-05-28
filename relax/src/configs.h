f22996dac1318d1194bc3c0a18222074919b4895
Shiruba <filipeps3net@hotmail.com>
2017-05-28 04:46:58 +0100

    Changed default keys to X and Z

--- relax/src/configs.h
+++ relax/src/configs.h
@@ -8,8 +8,8 @@ TCHAR newWindowTitle[MAX_PATH];
 char config_window_name = GetPrivateProfileString(L"Window", L"windowname", L"osu!relax", newWindowTitle, sizeof(newWindowTitle), L".\\config.ini");
 
 //keys
-char config_left_key = GetPrivateProfileInt(L"Keys", L"leftkey", VK_NUMPAD8, L".\\config.ini");
-char config_right_key = GetPrivateProfileInt(L"Keys", L"rightkey", VK_NUMPAD7, L".\\config.ini");
+char config_left_key = GetPrivateProfileInt(L"Keys", L"leftkey", 0x58, L".\\config.ini");
+char config_right_key = GetPrivateProfileInt(L"Keys", L"rightkey", 0x5A, L".\\config.ini");
 
 //delays
 int config_delaystartmin = GetPrivateProfileInt(L"DelayStart", L"min", 0, L".\\config.ini");
